0ee080837cd9c597aecc2364083ac4f0ac03afd0
Junayed Mahmud <junayedmahmud@yahoo.com>
2019-01-24 04:12:26 +0600

    Create summation_of_divisors.cpp

---
+++ summation_of_divisors.cpp
@@ -0,0 +1,123 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef vector<int>vi;
+
+ll sieve_size;
+vi primes;
+
+bitset<100000010>bs;
+
+void sieve(ll size_sieve)
+{
+    sieve_size=size_sieve+1;
+    bs.set();
+
+    bs[0]=bs[1]=0;
+
+    for(ll i=0;i<=sieve_size;i++)
+    {
+        if(bs[i])
+        {
+            for(ll j=i*i;j<=sieve_size;j=j+i)
+            {
+                bs[j]=0;
+            }
+
+            primes.push_back((int)i);
+        }
+    }
+}
+
+/*void prime_factors(ll N)
+{
+    ll factor_index=0;
+    ll pf=primes[factor_index];
+
+    while(pf*pf<=N)
+    {
+        while(N%pf==0)
+        {
+            N=N/pf;
+
+            cout<<pf<<" ";
+        }
+        pf=primes[++factor_index];
+    }
+
+    if(N!=1)
+    {
+        cout<<N<<endl;
+    }
+}*/
+
+/*ll divisors(ll n)
+{
+    ll t=0,pf=primes[t],ans=1;
+
+    while(pf*pf<=n)
+    {
+        ll power=0;
+
+        while(n%pf==0)
+        {
+            n=n/pf;
+            power=power+1;
+        }
+
+        ans=ans*(power+1);
+        pf=primes[++t];
+    }
+
+    if(n!=1)
+    {
+        ans=ans*2;
+    }
+
+    return ans;
+}*/
+
+ll sumdiv(ll n)
+{
+    ll t=0,pf=primes[t],ans=1;
+
+    while(pf*pf<=n)
+    {
+
+        ll power=0;
+
+        while(n%pf==0)
+        {
+            n=n/pf;
+            power=power+1;
+        }
+
+        ans=ans*((ll)pow((double)pf,power+1.0)-1)/(pf-1);
+        cout<<ans<<endl;
+        pf=primes[++t];
+    }
+
+    if(n!=1)
+    {
+        ll r=(ll)(pow((double)n,(double)2.0));
+        ll l=n-1;
+
+        ll result=r/l;
+
+        ans=ans*result;
+    }
+
+    return ans;
+}
+
+int main()
+{
+    sieve(10000000);
+
+    ll result=sumdiv(60);
+
+    cout<<result<<endl;
+
+    return 0;
+}
